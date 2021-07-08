7c8f5732a3b88215f5424e0b0d7b90d5c689fa0b
Esther <yunalesca002@gmail.com>
2021-07-08 22:46:38 +0100

    Cleaning code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//Feature 1 - Date and Time
+//Date and Time
 let now = new Date();
 
 let dateTime = document.querySelector("#date-time");
@@ -16,59 +16,23 @@ let day = days[now.getDay()];
 
 dateTime.innerHTML = `${day}  ${hours}:${minutes}`;
 
-//Feature 2 - Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
-//function signUp(event) {
-//  event.preventDefault();
-// let input = document.querySelector("#form-input");
-// let currentCity = document.querySelector("#current-city");
-// currentCity.innerHTML = `${input.value}`;
-//}
-//let form = document.querySelector("#form-search");
-//form.addEventListener("submit", signUp);
-
-//BONUS! Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit.
-
-//When clicking on Celsius, it should convert it back to Celsius.
-//function temperatureDisplayCelcius(event) {
-//event.preventDefault();
-//let currentTemperatureC = document.querySelector("#temperatureNow");
-//currentTemperatureC.innerHTML = 17;
-//}
-
-//let celciusLink = document.querySelector("#celsius-link");
-//celciusLink.addEventListener("click", temperatureDisplayCelcius);
-
-// When clicking on it, it should convert the temperature to Fahrenheit.
-
-//function temperatureDisplayFahrenheit(event) {
-//event.preventDefault();
-// let currentTemperatureFahrenheit = document.querySelector("#temperatureNow");
-// currentTemperatureFahrenheit.innerHTML = 62;
-//}
-
-//let fahrenheitLink = document.querySelector("#fahrenheit-link");
-//fahrenheitLink.addEventListener("click", temperatureDisplayFahrenheit);
-
-//when a user searches for a city (example: New York),
-//it should display the name of the city on the result page and the current temperature of the city.
-
-//Challenge 1
+//Show weather characteristics
 function showWeatherNow(response) {
   let weatherSpan = document.querySelector("#temperatureNow");
   let temperature = Math.round(response.data.main.temp);
   weatherSpan.innerHTML = temperature;
   if (temperature > 15) {
     let greeting = document.querySelector("#message");
-    greeting.innerHTML = "Don't forget to drink water!";
+    greeting.innerHTML = "Don't forget to drink water!💦";
     let mainImage = document.querySelector("#penguin-image");
     mainImage.src = "images/yellowpenguin2.gif";
   } else {
     let greeting = document.querySelector("#message");
-    greeting.innerHTML = "Don't forget your coat!";
+    greeting.innerHTML = "Don't forget your scarf!🧣";
     let mainImage = document.querySelector("#penguin-image");
     mainImage.src = "images/autumnpenguin2.gif";
   }
-  //Name
+  //Name of the city
   let currentCity = document.querySelector("#current-city");
   let cityName = response.data.name;
   currentCity.innerHTML = cityName;
@@ -80,6 +44,10 @@ function showWeatherNow(response) {
   let windSelection = document.querySelector("#current-wind");
   let wind = Math.round(response.data.wind.speed);
   windSelection.innerHTML = wind;
+  //Weather description
+  let descriptionElement = document.querySelector("#description-weather");
+  let weatherName = response.data.weather[0].description;
+  descriptionElement.innerHTML = weatherName;
 }
 
 function singUp(event) {
@@ -96,7 +64,7 @@ function singUp(event) {
 let form = document.querySelector("#form-search");
 form.addEventListener("submit", singUp);
 
-//Challenge 2
+//Current position
 function handlePosition(position) {
   let apiKey = "c34aa44e18e48aaa2ad40e6619e01c0c";
   let lat = position.coords.latitude;
